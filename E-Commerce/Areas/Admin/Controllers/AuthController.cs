50f2c2c27dd43fb0b5f03328d60966318312644f
fdat3 <datp0628@gmail.com>
2022-11-14 08:04:58 +0700

    Add file BackupDatabase, and edit file partial to Partial Folder

---
+++ E-Commerce/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Admin/Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            
+            return View();
+        }
+
+        public ActionResult ForgetPW()
+        {
+            return View();
+        }
+    }
+}
